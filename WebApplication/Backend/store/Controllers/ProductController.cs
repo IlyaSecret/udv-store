3b532e7646586de542bd3fbcc33d880ce50d5978
IlyaSecret <ilyakosyakov1@yandex.ru>
2022-05-03 19:04:34 +0500

    update

---
+++ WebApplication/Backend/store/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using store.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace store.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {       
+        [HttpGet]
+        public IEnumerable<Products> Get()
+        {
+            using (var context = new udvstoreContext())
+            {
+                return context.Products.ToList();
+            }
+        }
+    }
+}
---
+++ WebApplication/Backend/store/Models/Products.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace store.Models
+{
+    public partial class Products
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public int? Price { get; set; }
+        public string ProductDescription { get; set; }
+        public bool? InExistance { get; set; }
+        public string Img { get; set; }
+        public string ProductSize { get; set; }
+    }
+}
