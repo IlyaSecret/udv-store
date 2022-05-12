e37c6ea32202e18c80874c8d12a09b0d52c47ba5
Марк <markshubat@gmail.com>
2022-05-12 20:35:14 +0500

    добавил заказы, пока без изменения статуса и с добавлением по строке через пробел

---
+++ WebApplication/Controllers/MessagesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class MessagesController : ControllerBase
+    {
+       [HttpGet]
+       [Route("Get")]
+        public IEnumerable<Messages> Get()
+        {
+            var context = new udvstoreContext();
+            return context.Messages.ToList();
+        }
+
+        [HttpPost]
+        [Route("SendMessage")]
+        public IActionResult SendMessage(int type, string comment)
+        {
+            var context = new udvstoreContext();
+            var message = new Messages { Fio = Startup.currentUser.Fio, Email = Startup.currentUser.Email, Comment = comment, RequestType = type };
+            context.Messages.Add(message);
+            context.SaveChangesAsync();
+            return Ok("Запрос на получение UCoin отправлен");
+        }
+    }
+}
---
+++ WebApplication/Models/Orders.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplication1.Models
+{
+    public partial class Orders
+    {
+        public int Id { get; set; }
+        public string Fio { get; set; }
+        public string Items { get; set; }
+        public int? OrderStatus { get; set; }
+    }
+}
