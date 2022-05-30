a354f3fb05db80fa0c634c7561f0180dc4841fd1
ReyyL <lazarev.andrei@urfu.me>
2022-05-30 20:34:02 +0500

    Delete names.ts

--- WebApplication/ClientApp/src/utils/names.ts
+++
@@ -1,5 +0,0 @@
-import { Console } from "console";
-import { RootStateOrAny, useSelector } from "react-redux";
-
-const user = useSelector((state:RootStateOrAny)=>state.user.user)
-export const [lastName, firstName, patronymic] = user.fio?.split(' ')
